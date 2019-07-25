8b698c5a4eb5dd9bd86eac61c1a3783d8d26b190
Nileshraut607 <33259696+Nileshraut607@users.noreply.github.com>
2019-07-25 23:30:22 +0530

    Add files via upload

---
+++ controller/RegisterController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using Web_Asp_Spam.Models;
+using System.Web.UI;
+using System.Data.Entity.Validation;
+
+namespace Web_Asp_Registration.Controllers
+{
+    public class RegisterController : Controller
+    {
+        emaildbEntities1 db = new emaildbEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult SaveData(UserData model)
+        {
+            model.IsValid = "false";
+            db.UserDatas.Add(model);
+            try
+            {
+
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            BuildEmailTemplate(model.Id);
+            return Json("Registration Successful", JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult Confirm(int regId)
+        {
+            ViewBag.regID = regId;
+            return View();
+        }
+        public JsonResult RegisterConfirm(int regId)
+        {
+            UserData Data = db.UserDatas.Where(x => x.Id == regId).FirstOrDefault();
+            Data.IsValid = "true";
+            db.SaveChanges();
+            var msg = "Your Email Is Verified!";
+            return Json(msg, JsonRequestBehavior.AllowGet);
+        }
+
+        private void BuildEmailTemplate(int regID)
+        {
+            string body = System.IO.File.ReadAllText(HostingEnvironment.MapPath("~/EmailTemplate/") + "Text" + ".cshtml");
+            var regInfo = db.UserDatas.Where(x => x.Id == regID).FirstOrDefault();
+            var url = "https://localhost:44394/" + "Register/Confirm?regId=" + regID;
+            body = body.Replace("@ViewBag.ConfirmationLink", url);
+            body = body.ToString();
+            BuildEmailTemplate("Your Account Is Successfully Created", body, regInfo.Email_ID);
+        }
+
+        public static void BuildEmailTemplate(string subjectText, string bodyText, string sendTo)
+        {
+            string from, to, bcc, cc, subject, body;
+            from = "systemmailverify1@gmail.com";
+            to = sendTo.Trim();
+            bcc = "";
+            cc = "";
+            subject = subjectText;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(bodyText);
+            body = sb.ToString();
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(from);
+            mail.To.Add(new MailAddress(to));
+            if (!string.IsNullOrEmpty(bcc))
+            {
+                mail.Bcc.Add(new MailAddress(bcc));
+            }
+            if (!string.IsNullOrEmpty(cc))
+            {
+                mail.CC.Add(new MailAddress(cc));
+            }
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            SendEmail(mail);
+        }
+
+        public static void SendEmail(MailMessage mail)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new System.Net.NetworkCredential("systemmailverify1@gmail.com", "qwerty12345@xyz");
+            try
+            {
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public JsonResult CheckValidUser(UserData model)
+        {
+            string result = "Fail";
+            var DataItem = db.UserDatas.Where(x => x.New_Mail == model.Email_ID && x.Password == model.Password && x.IsValid=="true").SingleOrDefault();
+            if (DataItem != null)
+            {
+                Session["UserID"] = DataItem.Id.ToString();
+                Session["UserName"] = DataItem.New_Mail.ToString();
+                result = "Success";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult AfterLogin()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+        
+    }
+    
+
+}
