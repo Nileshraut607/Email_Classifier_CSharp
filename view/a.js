036ce6f5a2e066cbd3b5ec3df04e712dc0992490
Nileshraut607 <33259696+Nileshraut607@users.noreply.github.com>
2019-07-25 23:29:16 +0530

    Add files via upload

---
+++ view/a.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $('#grid_inbox').hide();
+    $('#grid_home').show();
+    $('#grid_spam').hide();
+
+    $('#a1').click(function (e) {
+
+        e.preventDefault();
+        $('#grid_inbox').hide();
+        $('#grid_home').show();
+        $('#grid_spam').hide();
+    });
+    $('#a2').click(function (e) {
+
+        e.preventDefault();
+        $('#grid_inbox').show();
+        $('#grid_home').hide();
+        $('#grid_spam').hide();
+    });
+    $('#a3').click(function (e) {
+
+        e.preventDefault();
+        $('#grid_inbox').hide();
+        $('#grid_home').hide();
+        $('#grid_spam').show();
+    });
+
+});
